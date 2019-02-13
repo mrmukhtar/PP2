601167ad6c4394f24814c5a5e391f53bb252d87e
Mukhtar <amirkumarmukhtar@gmail.com>
2019-02-13 17:37:35 +0600

    Create Task3.cs

---
+++ week1/Task3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine()); // первый элемент считал и перевел в int
+            string s = Console.ReadLine();
+            string[] a = s.Split(); // поделил строки через пробел и залил в массив
+
+            Console.WriteLine();
+            for (int i = 0; i < x; i++)
+            {
+                Console.Write(a[i] + " " + a[i] + " "); // вывел элемент дважды
+            }
+        }
+    }
+}
