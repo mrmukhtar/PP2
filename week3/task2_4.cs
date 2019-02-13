1be9cdaabbc827793292477a61742871c9f475c7
Mukhtar <amirkumarmukhtar@gmail.com>
2019-02-13 17:40:49 +0600

    Create task2_4.cs

---
+++ week3/task2_4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab3_2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.Move(@"C: \Users\User\Desktop\Damn\C#\MYY.txt", @"C:\Users\User\Desktop\Damn\C#\Newnew.txt"); // переименовывать файл
+        }
+    }
+}
