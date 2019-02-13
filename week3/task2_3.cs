903dd6d977a1d0e3a6f2dda0809f0be88271880f
Mukhtar <amirkumarmukhtar@gmail.com>
2019-02-13 17:40:41 +0600

    Create task2_3.cs

---
+++ week3/task2_3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab3_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = File.OpenText(@"C:\Users\User\Desktop\Damn\C#\MYY.txt");
+            string s = "";
+            while ((s = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
