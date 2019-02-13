f1e41d05d4f6e4bfcb0ae0d983ab07bc4473f8d2
Mukhtar <amirkumarmukhtar@gmail.com>
2019-02-13 17:40:14 +0600

    Create task2_2.cs

---
+++ week3/task2_2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab3_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.Delete(@"C:\Users\User\Desktop\Damn\C#\LB\MYY.txt"); //задаем путь и удаляем выбранный файл 
+
+            Directory.Delete(@"C:\Users\User\Desktop\Damn\C#\LB\Lab1"); // задаем путь и удаляем выбранную папку но она должна быть пуста
+        }
+    }
+}
