2f1d1ee9bfe12756c370bf37b422c50ca0ae2126
Mukhtar <amirkumarmukhtar@gmail.com>
2019-02-14 14:25:09 +0600

    Update task4.cs

--- week2/task4.cs
+++ week2/task4.cs
@@ -12,15 +12,16 @@ namespace Lab2_4_2
         static void Main(string[] args)
         {
             string fileName = "MyText.txt"; // имя файла который я хочу создать
-            string PathString = @"C:\Users\User\Desktop\Damn\C#"; // и где я его хочу создать
+            string PathString = @"C:\Users\Mukhtar\Desktop\PP2\PP2\week2"; // и где я его хочу создать
+
             PathString = Path.Combine(PathString, fileName); // нужно скомбинировать их пути
             FileStream fs = File.Create(PathString); // и создаем
             fs.Close(); // нужно что бы прекратил работу
 
-            string PathString2 = @"C:\Users\User\Desktop\Damn\C#\LB"; // путь куда я должен я скопировать
+            string PathString2 = @"C:\Users\Mukhtar\Desktop\PP2\PP2"; // путь куда я должен я скопировать
 
-            string destFile = System.IO.Path.Combine(PathString2, fileName); // скомбинируем путь
-            File.Copy(PathString, destFile, true); // копируем туда
+            string destFile = Path.Combine(PathString2, fileName); // скомбинируем путь
+            File.Copy(PathString, destFile); // копируем туда
 
             File.Delete(PathString); // и удаляем оригинал
 
